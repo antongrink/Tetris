b35d58da544f786f676f55cef32b0ba35871a8b2
l <you@example.com>
2022-07-31 09:10:27 +0300

    Добавьте файлы проекта.

---
+++ SameGame/SameGameDoc.cpp
@@ -0,0 +1,33 @@
+﻿#include "SameGame.h"
+#include "pch.h"
+#include "SameGameDoc.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CSameGameDoc
+IMPLEMENT_DYNCREATE(CSameGameDoc, CDocument)
+BEGIN_MESSAGE_MAP(CSameGameDoc, CDocument)
+END_MESSAGE_MAP()
+
+// Создание CSameGameDoc
+CSameGameDoc::CSameGameDoc()
+{
+}
+
+// Уничтожение CSameGameDoc
+CSameGameDoc::~CSameGameDoc()
+{
+}
+
+BOOL CSameGameDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// Установка (или сброс) параметров доски
+	m_board.SetupBoard();
+
+	return TRUE;
+}
